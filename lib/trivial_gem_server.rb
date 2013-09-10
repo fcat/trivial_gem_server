2108b0b54a32cd7caf93b6cd7f35472692afe9c3
Fabien Catteau <fabien.catteau@mingalar.fr>
2013-09-10 23:53:44 +0200

    Make it work, but we still miss many tests

--- lib/trivial_gem_server.rb
+++ lib/trivial_gem_server.rb
@@ -1,64 +1,116 @@
 require 'rubygems'
+require 'debugger'
 require 'sinatra/base'
+require 'syck'
+require 'yaml'
+
+module YAML
+  module Syck
+    class DefaultKey
+    end
+  end
+end
+
+module TrivialGemServer
+end
 
 class TrivialGemServer::Server < Sinatra::Base
-  attr_reader :gem_dirs
+  configure :production, :development do
+    set :gem_dirs, Array(ENV['GEM_DIR'] || Gem.path)
+  end
 
-  def initialize *gem_dirs
-    @gem_dirs = gem_dirs || Gem.path
-    Gem::Specification.dirs = gem_dirs
+  configure :test do
+    set :gem_dirs, Array(File.expand_path('../../test/fixtures/gems', __FILE__))
+  end
+
+  before do
+    # don't set up gem_dirs in configure!
+    # you would loose your gem set and fallback to webrick!
+    Gem::Specification.dirs = settings.gem_dirs
+  end
+
+  get '/' do
+    render_text_for_specs all_specs
+  end
+
+  get '/specs.txt' do
+    render_text_for_specs all_specs
+  end
+
+  get '/latest_specs.txt' do
+    render_text_for_specs latest_specs
   end
 
   get '/specs.4.8.gz' do
-    handle_specs_request do
-      Gem::Specification.to_a
-    end
+    send_gip_for_specs all_specs
   end
 
   get '/latest_specs.4.8.gz' do
-    handle_specs_request do
-      Gem::Specification.latest_specs
-    end
+    send_gip_for_specs latest_specs
   end
 
-  get '/gems/:basename' do
-    if path = find_package_path(params[:basename])
-      # TODO send file
+  get '/gems/:filename' do
+    if path = find_package_path(params[:filename])
+      send_file path
     else
-      # 404
+      halt 404, "#{params[:filename]} not found!"
     end
   end
 
   get '/quick/Marshal.4.8/:fullname.gemspec.rz' do
     specs = find_specs_by_fullname(params[:fullname])
-
     if specs.empty? then
-      # TODO
-      # res.status = 404
-      # res.body = "Gem not found"
+      halt 404, "Gem #{params[:fullname]} not found"
     elsif specs.length > 1 then
-      # TODO
-      # res.status = 500
-      # res.body = "Multiple gems found matching"
-    elsif marshal_format then
-      # TODO
-      # res['content-type'] = 'application/x-deflate'
-      body = Gem.deflate(Marshal.dump(specs.first))
-      # send body
+      halt 500, "Multiple gems found when only one expected!"
+    else
+      send_rz_for_spec specs.first
     end
   end
 
   private
 
+  def all_specs
+    Gem::Specification.to_a
+  end
+
+  def latest_specs
+    Gem::Specification.latest_specs
+  end
+
+  def render_text_for_specs specs
+    headers 'content-type' => 'text/plain'
+    specs.map do |s|
+      [s.name, s.version.to_s].join('-')
+    end.join("\n")
+  end
+
+  def send_gip_for_specs(specs)
+    overview = specs.sort_by(&:sort_obj).map do |spec|
+      platform = spec.original_platform || Gem::Platform::RUBY
+      [spec.name, spec.version.to_s, platform]
+    end
+    headers 'content-type' => 'application/x-gzip'
+    Gem.gzip(Marshal.dump(overview))
+  end
+
   def find_package_path(basename)
-    catch(:path) do
-      cache_dirs.each do |dir|
-        path = File.join dir,
-          if File.exists? path
-            throw :path, path
-          end
+    cache_dirs.each do |dir|
+      path = File.join dir, basename
+      if File.exists? path
+        return path
       end
     end
+    nil
+  end
+
+  def cache_dirs
+    settings.gem_dirs.map { |gem_dir| File.join gem_dir, 'cache' }
+  end
+
+  def send_rz_for_spec spec
+    headers 'mime-type' => 'application/x-deflate'
+    Gem.deflate(Marshal.dump(spec))
   end
 
   def find_specs_by_fullname(fullname)
@@ -75,43 +127,9 @@ class TrivialGemServer::Server < Sinatra::Base
   end
 
   def find_specs(name, version, platform)
-    Gem::Specification.reset
     specs = Gem::Specification.find_all_by_name name, version
-    specs.select { |s| s.platform == platform }
+    specs.select! { |s| s.platform == platform }
+    specs
   end
 
-  def handle_specs_request
-    # Gem::Specification.reset
-
-    specs = yield.sort_by(&:sort_obj).map do |spec|
-      platform = spec.original_platform || Gem::Platform::RUBY
-      [spec.name, spec.version, platform]
-    end
-
-    headers = {'content-type' => 'application/x-gzip'}
-    zipped_specs = Gem.gzip(Marshal.dump(specs))
-    [200, headers, zipped_specs]
-  end
-
-  def cache_dirs
-    gem_dirs.map { |gem_dir| File.join gem_dir, 'cache' }
-  end
-
-  def spec_dirs
-    gem_dirs.map { |gem_dir| File.join gem_dir, 'specifications' }
-  end
-
-  def error_response(code, message)
-    halt [code, message] if api_request?
-    html = <<HTML
-<html>
-  <head><title>Error - #{code}</title></head>
-  <body>
-    <h1>Error - #{code}</h1>
-    <p>#{message}</p>
-  </body>
-</html>
-HTML
-    halt [code, html]
-  end
 end
