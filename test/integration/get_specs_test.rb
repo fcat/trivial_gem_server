2addd47af19c2c4a1ed6a810f4838f6b4c45f94b
Fabien Catteau <fabien.catteau@mingalar.fr>
2013-09-11 07:48:13 +0200

    Add tests for *specs.txt and refactor

--- test/integration/get_specs_test.rb
+++ test/integration/get_specs_test.rb
@@ -3,10 +3,40 @@ require 'test_helper'
 class ServeSpecsTest < TrivialGemServer::TestCase
   include Rack::Test::Methods
 
-  def test_it_serve_all_specs
+  def test_it_serve_all_specs_as_text
+    get '/specs.txt'
+    assert last_response.ok?
+    assert_equal 'text/plain', content_type
+    assert_equal "builder-3.2.2
+rack-1.4.0
+rack-1.3.0
+rack-1.5.0
+rack-protection-1.5.0
+rack-test-0.6.2
+sinatra-1.3.3
+sinatra-1.4.3
+tilt-1.4.1".split.sort,
+      last_response.body.split.sort
+  end
+
+  def test_it_serve_latest_specs_as_text
+    get '/latest_specs.txt'
+    assert last_response.ok?
+    assert_equal 'text/plain', content_type
+    assert_equal last_response.body.split.sort,
+"builder-3.2.2
+rack-1.5.0
+rack-protection-1.5.0
+rack-test-0.6.2
+sinatra-1.4.3
+tilt-1.4.1".split.sort
+  end
+
+  def test_it_serve_all_specs_as_gzip
     get '/specs.4.8.gz'
     assert last_response.ok?
-    assert_equal extract_zipped_specs(last_response.body), [
+    assert_equal 'application/x-gzip', content_type
+    assert_equal [
       ["builder", "3.2.2", "ruby"],
       ["rack", "1.4.0", "ruby"],
       ["rack", "1.3.0", "ruby"],
@@ -15,24 +45,32 @@ class ServeSpecsTest < TrivialGemServer::TestCase
       ["rack-test", "0.6.2", "ruby"],
       ["sinatra", "1.3.3", "ruby"],
       ["sinatra", "1.4.3", "ruby"],
-      ["tilt", "1.4.1", "ruby"]].sort
+      ["tilt", "1.4.1", "ruby"]].sort,
+      extract_zipped_specs
   end
 
-  def test_it_serve_latest_specs
+  def test_it_serve_latest_specs_as_gzip
     get '/latest_specs.4.8.gz'
     assert last_response.ok?
-    assert_equal extract_zipped_specs(last_response.body), [
+    assert_equal 'application/x-gzip', content_type
+    assert_equal [
       ["builder", "3.2.2", "ruby"],
       ["rack", "1.5.0", "ruby"],
       ["rack-protection", "1.5.0", "ruby"],
       ["rack-test", "0.6.2", "ruby"],
       ["sinatra", "1.4.3", "ruby"],
-      ["tilt", "1.4.1", "ruby"]].sort
+      ["tilt", "1.4.1", "ruby"]].sort,
+      extract_zipped_specs
   end
 
   private
 
-  def extract_zipped_specs(raw)
+  def content_type
+    last_response.content_type.split(';')[0]
+  end
+
+  def extract_zipped_specs
+    raw = last_response.body
     Marshal.load(Gem.gunzip(raw)).sort
   end
 
